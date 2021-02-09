3de9bf0c39baac98472860ae2d68b4b4e57a34be
pennd44 <pdiba@oxbowschool.org>
2021-02-08 19:35:08 -0500

    lab complete

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using en = EaterNamespace;
+using mn=MyNamespace;
+
+namespace MyNamespace
+{
+    public class AnotherProgram
+    {
+        public static void PrintText()
+        {
+            Console.WriteLine("Hello Earth!");
+        }
+    }
+}
+namespace EaterNamespace
+{
+    public class Eater
+    {
+        public static void Eat()
+        {
+            Console.WriteLine("Munch Munch Munch");
+        }
+    }
+}
+
+
+namespace AnotherProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            mn.AnotherProgram.PrintText();
+            en.Eater.Eat();
+        }
+    }
+}
